35d7d0d3bd1b0d93099b76077418819cc2cea71c
Sadullah GOVENC <48141225+SadullahGovenc@users.noreply.github.com>
2023-12-16 17:13:20 +0300

    Oyuna 4 bölüm getirildi.

--- Land It/Assets/Scripts/RocketController.cs
+++ Land It/Assets/Scripts/RocketController.cs
@@ -3,27 +3,146 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RocketController : MonoBehaviour
 {
     [SerializeField]
     private float force_Amount = 50f, rotate_Amount = 100f;
     [SerializeField]
     private AudioClip rocket_Sound, win_Sound, lose_Sound;
     [SerializeField]
-    private ParticleSystem rocket_Particle, wim_Particle, lose_Particle;
+    private ParticleSystem rocket_Particle, win_Particle, lose_Particle;
     [SerializeField]
     private float transition_Time = 2f;
 
 
-    private enum State {WIN}
-    void Start()
+    private enum State { WIN, LOSE, ALIVE }
+    private State playerState = State.ALIVE;
+
+    private AudioSource audioSource;
+    private Rigidbody myBody;
+
+    private void Awake()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        myBody = GetComponent<Rigidbody>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerState == State.ALIVE)
+        {
+            HandleRotate();
+        }
     }
+
+    private void FixedUpdate()
+    {
+        if (playerState == State.ALIVE)
+        {
+            HandleMovement();
+        }
+    }
+
+    void HandleRotate()
+    {
+        myBody.freezeRotation = true;
+
+        float rotationThisFrame = rotate_Amount * Time.deltaTime;
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        myBody.freezeRotation = false;
+    }
+    void HandleMovement()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myBody.AddRelativeForce(Vector3.up * force_Amount);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(rocket_Sound);
+            }
+            rocket_Particle.Play();
+
+        }
+        else
+        {
+            rocket_Particle.Stop();
+        }
+
+    }
+    void LevelFinished()
+    {
+        playerState = State.WIN;
+        audioSource.Stop();
+        audioSource.PlayOneShot(win_Sound);
+        win_Particle.Play();
+
+        Invoke("LoadNextLevel", transition_Time);
+    }
+
+    void PlayerDied()
+    {
+        playerState = State.LOSE;
+        audioSource.Stop();
+        audioSource.PlayOneShot(lose_Sound);
+        lose_Particle.Play();
+
+        Invoke("RestartLevel", transition_Time);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentScene_Plus_nextScene = SceneManager.GetActiveScene().buildIndex;
+        currentScene_Plus_nextScene += 1;
+        int count = SceneManager.sceneCountInBuildSettings;
+        if (currentScene_Plus_nextScene == count)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(currentScene_Plus_nextScene);
+        }
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+    private void OnCollisionEnter(Collision target)
+    {
+        if (playerState != State.ALIVE)
+        {
+            return;
+        }
+        switch (target.gameObject.tag)
+        {
+
+            case "Friendly":
+                break;
+            case "Finish":
+                LevelFinished();
+                break;
+
+            default:
+                PlayerDied();
+                break;
+
+        }
+
+    }
+
 }
+
